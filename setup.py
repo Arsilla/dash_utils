647f2119c693e20221e8cfdebb41de6635b40a4d
Susanne Groothuis <groothuis.susanne@kpmg.nl>
2019-01-09 22:31:34 +0100

    Initial commit

---
+++ setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+    name="dash_utils",
+    version="0.1.0",
+    python_requires=">=3.6",
+    packages=['dash_utils'],
+    py_modules=['dash_utils.dash_utils'],
+    author='Susanne Groothuis <groothuis.susanne@kpmg.nl>'
+
+)
